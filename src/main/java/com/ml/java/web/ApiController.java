3bdbdb4256474a120abb7f4a61c0f6227debaeff
John Paul Ada <adaj@ph.ibm.com>
2019-09-18 11:25:38 +0800

    feat: created base spring boot web

---
+++ src/main/java/com/ml/java/web/ApiController.java
@@ -0,0 +1,16 @@
+package com.ml.java.web;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.stereotype.Service;
+
+@Service
+@RestController
+public class ApiController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
+
+}
