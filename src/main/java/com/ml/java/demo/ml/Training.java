52164da59457202635e419de389e803a8796cb16
John Paul Ada <johnpaulada@gmail.com>
2019-09-20 07:33:20 +0800

    fix: removed try catch

--- src/main/java/com/ml/java/demo/ml/Training.java
+++ src/main/java/com/ml/java/demo/ml/Training.java
@@ -1,25 +1,7 @@
 package com.ml.java.demo.ml;
 
 import lombok.extern.slf4j.Slf4j;
-import org.deeplearning4j.datasets.datavec.RecordReaderDataSetIterator;
-import org.deeplearning4j.datasets.iterator.impl.TinyImageNetDataSetIterator;
-import org.deeplearning4j.nn.api.OptimizationAlgorithm;
-import org.deeplearning4j.nn.conf.distribution.NormalDistribution;
-import org.deeplearning4j.nn.conf.layers.OutputLayer;
-import org.deeplearning4j.nn.graph.ComputationGraph;
-import org.deeplearning4j.nn.transferlearning.FineTuneConfiguration;
-import org.deeplearning4j.nn.transferlearning.TransferLearning;
-import org.deeplearning4j.nn.weights.WeightInit;
-import org.deeplearning4j.zoo.PretrainedType;
-import org.deeplearning4j.zoo.ZooModel;
-import org.deeplearning4j.zoo.model.VGG16;
-import org.nd4j.linalg.activations.Activation;
-import org.nd4j.linalg.dataset.api.iterator.DataSetIterator;
-import org.nd4j.linalg.learning.config.Nesterovs;
-import org.nd4j.linalg.lossfunctions.LossFunctions;
 import org.springframework.stereotype.Service;
-
-import java.io.IOException;
 
 @Slf4j
 @Service
@@ -34,10 +16,5 @@ public class Training {
 
     public void train() {
         log.info("Started training");
-        try {
-            
-        } catch (IOException ex) {
-            log.error(ex.getMessage());
-        }
     }
 }
